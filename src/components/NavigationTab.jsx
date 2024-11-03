d965ce0f238eae81cfef7fd4464606f70ff6b859
Thirat <100107926+Thiratpyz@users.noreply.github.com>
2024-11-03 17:44:37 +0700

    Update NavigationTab.jsx

--- src/components/NavigationTab.jsx
+++ src/components/NavigationTab.jsx
@@ -111,7 +111,7 @@ function NavigationTab() {
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-transparent">
             <li>
               <div
-                className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 transition-transform transform  hover:shadow-lg cursor-pointer flex items-center relative"
+                className="block py-2 px-3 text-amber-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 transition-transform transform  hover:shadow-lg cursor-pointer flex items-center relative"
                 onClick={handleUserDataClick}
               >
                 {user ? (
