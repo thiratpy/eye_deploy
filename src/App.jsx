995c94ee665ac0ceee29ee1966c896fe9faa3061
Thiratpy <100107926+thiratpy@users.noreply.github.com>
2025-06-12 01:55:49 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WelcomeMenu from './components/WelcomeMenu';
-import Footer from './components/Footer'; // Assuming you already have this component
+import Footer from './components/Footer'; 
 import UploadRetinalImage from './components/UploadRetinalImage';
 import LoadingScreen from './components/LoadingScreen';
 import ResultsPage from './components/ResultsPage';
